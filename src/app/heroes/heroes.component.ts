ca1e0817b15fd34081985e8b252a8d2066a98639
Marvin Cayetano <cayetanomarvin@gmail.com>
2022-01-30 21:27:20 -0500

    🔥 Chore: NG FOR really like the ERB

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Hero } from '../Hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -7,10 +7,8 @@ import { Hero } from '../Hero';
   styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm',
-  };
+  heroes = HEROES;
+
   constructor() {}
 
   ngOnInit(): void {}
